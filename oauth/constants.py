9a59e5f90074555458594124db083c9b7f66fb44
dbosnjakovic <Dusan_Bosnjakovic@intuit.com>
2023-10-16 14:58:41 -0700

    API abstraction

---
+++ oauth/constants.py
@@ -0,0 +1,20 @@
+client_secrets = {
+    "client_id": "AB4pWaL6jKbGGqh8MLep",
+    "client_secret": "QrzMIqn9KSc2uVr7O",
+    "redirect_uri": "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl",
+    "environment": "sandbox"
+}
+
+qBooksAssets = {
+    "authentication_assets" : {
+        "realm_id": 4620816365348440870,
+        "auth_code": "AB116974dCULL0t0Ydd6R0mIcO"
+    }
+}
+
+refreshToken = "AB11706210119Fj8SmmOd1Jjpq7O"
+
+qBData = {
+    "realm_id":"4620816365348440870",
+    "accessToken":"FPZ5En5qHKHsuhihj8SmmOd1"
+}
